90995e59653c73c8c35363f2fd643070883b2dfb
st0rm <inthishouseofcards@gmail.com>
2023-11-24 03:18:22 -0500

    it probably works

---
+++ README.md
@@ -0,0 +1,8 @@
+# tBunnyEnabler
+
+> Enable the bunny pet in Terraria without the Collector's Edition
+
+## Usage
+
+1. Run the program
+2. If it says `Done!`, it was successful
---
+++ tBunnyEnabler/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace tBunnyEnabler
+{
+    internal class Program
+    {
+        private const string registryEntry = @"Software\Terraria";
+
+        static void Main(string[] args)
+        {
+            using (RegistryKey terrariaKey = Registry.CurrentUser.OpenSubKey(registryEntry, writable: true))
+            {
+                if (terrariaKey == null)
+                {
+                    Console.WriteLine($"An error occurred reading the bunny value in '{registryEntry}'. Exiting.");
+                    return;
+                }
+                if ((string)terrariaKey.GetValue("Bunny") == "1")
+                {
+                    Console.WriteLine("The bunny value is already set. Exiting.");
+                    return;
+                }
+                terrariaKey.SetValue("Bunny", "1", RegistryValueKind.String);
+            }
+            Console.WriteLine("Done!");
+        }
+    }
+}
