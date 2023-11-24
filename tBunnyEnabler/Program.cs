02502b5c12b55247fd8a2da4972a42e9b69a6a92
st0rm <inthishouseofcards@gmail.com>
2023-11-24 03:19:48 -0500

    use readkey instead of writeline

--- tBunnyEnabler/Program.cs
+++ tBunnyEnabler/Program.cs
@@ -14,16 +14,19 @@ namespace tBunnyEnabler
                 if (terrariaKey == null)
                 {
                     Console.WriteLine($"An error occurred reading the bunny value in '{registryEntry}'. Exiting.");
+                    Console.ReadKey();
                     return;
                 }
                 if ((string)terrariaKey.GetValue("Bunny") == "1")
                 {
                     Console.WriteLine("The bunny value is already set. Exiting.");
+                    Console.ReadKey();
                     return;
                 }
                 terrariaKey.SetValue("Bunny", "1", RegistryValueKind.String);
             }
             Console.WriteLine("Done!");
+            Console.ReadKey();
         }
     }
 }
