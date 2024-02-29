a1ae33fc446c8471de143138df684849ee8edfd9
frosty <inthishouseofcards@gmail.com>
2024-02-29 06:32:57 -0500

    rename and other crap

---
+++ Bunny/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace Bunny
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Terraria"))
+                {
+                    if (key != null)
+                    {
+                        key.SetValue("Bunny", "1");
+                        Console.WriteLine("Successfully given bunny access.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Unable to create or access registry key.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Bunny/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bunny")]
+[assembly: AssemblyDescription("Gives you access to the Bunny pet in Terraria.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("frostyfalls")]
+[assembly: AssemblyProduct("Bunny")]
+[assembly: AssemblyCopyright("Copyright © frostyfalls 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("47774c18-421a-45cd-a960-4d7150b91820")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
